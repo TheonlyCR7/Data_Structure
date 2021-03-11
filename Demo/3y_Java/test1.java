1b725b5ac979b6114630848456b5755a8f89106d
liuminchao123 <mr.liuminchao@outlook.com>
2021-03-11 21:25:51 +0800

    1

---
+++ Demo/3y_Java/test1.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class test1 {
+    public static void main(String args[]) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("key1", "1");
+        System.out.println(map.get("key1"));
+	
+    }
+}
